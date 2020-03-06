b7fd9313c272d7d8ba9ac3ea5aaf30687304cd4b
Ruma Das <rumaadl16@gmail.com>
2020-03-06 17:16:44 +1030

    Adding the hide button in javascript

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+//event building
+// selecting criteria list making a new const
+const criteriaList = document.querySelector('#pass-gen');
+const hideButton = document.querySelector("#hide-option").style.margin = "80px 10px 80px 10px";
+var hidden = false;
+ // for hiding the criteria to view
+ 
+const hideBox =document.querySelector('#hide-option');
+hideBox.addEventListener('click', function(e){
+    if(hidden){
+      criteriaList.style.display = "none";
+       
+    }else{
+      criteriaList.style.display ="initial";
+      hideButton = document.querySelector("#hide-option").style.display = "none";
+    }
+});
+
+
+
+
+
+/*
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +36,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
